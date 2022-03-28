51d6bbbde280e49d3bd8a1ccb362ce8150746dc0
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-03-28 19:54:06 +0300

    created MessageList and MessageForm components

--- myapp/src/components/Message/index.jsx
+++ myapp/src/components/Message/index.jsx
@@ -1,9 +1,40 @@
-import './style.scss'
+import React, {useState} from 'react';
+import './style.scss';
+import MessageList from '../MessageList';
+import MessageForm from '../MessageForm';
+
+const name = "Antonio"
+
+let msgs = [
+    {
+        author: name,
+        text: 'If you want to change smt - start do it now!'
+    },
+    {
+        author: name,
+        text: 'But its not sure...'
+    },
+    {
+        author: name,
+        text: 'Smt else ...'
+    }
+]
 
 export default function Message({ text }) {
+    const [messages, setMessages] = useState(msgs);
+
+    const addMessage = () => {
+        // setMessages()
+        
+    };
+
     return (
         <div className="Message">
             <h1>{text}</h1>
+            {messages.map((msg) => 
+                <MessageList author={msg.author} text={msg.text}/>
+            )}
+            <MessageForm />
         </div>
     );
 }
---
+++ myapp/src/components/MessageForm/index.jsx
@@ -0,0 +1,9 @@
+export default function MessageForm(){
+    return (
+        <form action="#">
+            <button onClick={()=>{}}>Add message</button>
+        </form>
+    )
+}
+
+// stoped 1:35 lesson 2
