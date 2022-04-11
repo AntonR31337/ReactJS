6e5cb46a4c222a6adac02907e52f721d401aa3fc
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-04-11 17:23:54 +0300

    try to separete chats

--- myapp/src/components/MessageList/index.jsx
+++ myapp/src/components/MessageList/index.jsx
@@ -6,46 +6,51 @@ import { useParams } from 'react-router';
 
 const name = "Antonio"
 
-let msgs = [];
+// let msgs = [];
 
 export default function MessageList({ text }) {
     const { id } = useParams();
 
-    const [messages, setMessages] = useState(msgs);
+    const [messages, setMessages] = useState({});
 
     const addMessage = (newText) => {
-        setMessages([...messages, {
-            author: name,
-            text: newText,
-            id: `msg-${Date.now()}`
-        }
-        ])
-        console.log(messages);
+        debugger
+        setMessages({ ...messages, [id]: [...messages[id], newText] });
     };
 
-    useEffect(() => {
-        let timeOut;
-        if (messages.length && messages[messages.length - 1].author === name) {
-            timeOut = setTimeout(() => {
-                setMessages([...messages, {
-                    author: "Robot",
-                    text: "Сообщение проверено роботом",
-                    id: `msg-${Date.now()}`
-                }])
-            }, 1000)
-        }
-        return () => clearTimeout(timeOut);
-    }, [messages])
+    // const addMessage = (newText) => {
+    //     setMessages([...messages, {
+    //         author: name,
+    //         text: newText,
+    //         id: `msg-${Date.now()}`
+    //     }
+    //     ])
+    //     console.log(messages);
+    // };
+
+    // useEffect(() => {
+    //     let timeOut;
+    //     if (messages.length && messages[messages.length - 1].author === name) {
+    //         timeOut = setTimeout(() => {
+    //             setMessages([...messages, {
+    //                 author: "Robot",
+    //                 text: "Сообщение проверено роботом",
+    //                 id: `msg-${Date.now()}`
+    //             }])
+    //         }, 1000)
+    //     }
+    //     return () => clearTimeout(timeOut);
+    // }, [messages])
 
     return (
         <div className="Message">
             <h1>{text}</h1>
             <div id='MessageDisplay'>
-                <div className="MessageList">
+                {/* <div className="MessageList">
                     {messages.map((msg) =>
                         <Message key={msg.id} author={msg.author} text={msg.text} />
                     )}
-                </div>
+                </div> */}
             </div>
             <MessageForm onSubmit={addMessage} />
         </div>
--- myapp/src/components/Testing/index.jsx
+++ myapp/src/components/Testing/index.jsx
@@ -3,8 +3,14 @@ import React from "react";
 export const MyThemeContext = React.createContext({theme: 'dark'});
 
 export default function Testing() {
-    
 
+    // const mesgs = [
+    //     {chat1: [
+    //         {name: "name1", text: "text1"},
+    //         {name: "name2", text: "text2"}
+    //     ]}
+    // ];
+    // console.log(mesgs);
     return(
         <h1>Test component</h1>
     )
